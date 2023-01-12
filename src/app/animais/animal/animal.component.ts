c71b8a610320a7c1ff3ccfc553bf25aba28bb4ae
AnaHeline <anaheline@gmail.com>
2023-01-11 21:32:36 -0300

    list-A

--- src/app/animais/animal/animal.component.ts
+++ src/app/animais/animal/animal.component.ts
@@ -18,7 +18,7 @@ export class AnimalComponent implements OnInit{
     if(url.startsWith('data')){
       this.urlOriginal = url;
     }else{
-      this.urlOriginal = `${API}/imgs/${url}/`;
+      this.urlOriginal = `${API}/uploads/imgs/${url}/`;
     }
   }
 
